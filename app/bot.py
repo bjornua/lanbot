3464f57986a8165e135e2fed4b23dfc574e77a7e
Bjørn Uhre Arnhotlz <rugkiks@gmail.com>
2011-05-14 14:03:42 +0200

    derp

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import app.lib.irc
 from datetime import datetime
+from app.lib.string import parsecommand
+
+
 
 class LANBot(object):
     def __init__(self, nick):
@@ -8,6 +11,13 @@ class LANBot(object):
         self.client.event.add("chanmsg", self.onchanmsg)
         self.client.event.add("usermsg", self.onusermsg)
         self.client.parser.event.add("endofmotd", self.onendofmotd)
+        self.client.parser.event.add("line", self.onrecvline)
+        self.client.writer.event.add("line", self.onsendline)
+    
+    def onrecvline(self, line):
+        print "< " + line
+    def onsendline(self, line):
+        print "> " + line
 
     def onendofmotd(self, *args):
         print "Joining #dikulan"
@@ -17,10 +27,12 @@ class LANBot(object):
         msg = unicode(msg, "utf-8", "replace")
         date = datetime.now().strftime("%H:%M:%S")
         print u"%s - [%s][%s] %s" % (date, chan, fromnick, msg)
+        
+        if msg.startswith("!"):
+            parsed = parsecommand(msg[1:])
+            self.client.writer.msgline(chan, repr(parsed), blocking=False)
+            self.client.writer.msgline(chan, repr(parsed), blocking=False)
 
-        if fromnick == "freeload" and msg == "bla":
-            for x in range(10):
-                self.client.writer.msglines("#dikulan", [u"1",u"2",u"3"])
         
     def onusermsg(self, fromnick, fromuser, fromhost, msg):
         msg = unicode(msg, "utf-8", "replace")
